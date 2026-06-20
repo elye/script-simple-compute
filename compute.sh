8f5c76ae6c0746b918fbc569001086248d8b456c
elisha.lye <elisha.lye@rea-group.com>
2026-06-20 11:25:50 +1000

    add times & divide

--- compute.sh
+++ compute.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 if [ $# -ne 3 ]; then
-  echo "Usage: $0 <num1> <+|-> <num2>"
+  echo "Usage: $0 <num1> operator <num2>"
+  echo " Operator can be +, -, x or /"
   exit 1
 fi
 
 case "$2" in
   +) echo $(($1 + $3)) ;;
   -) echo $(($1 - $3)) ;;
+  x) echo $(($1 * $3)) ;;
+  /) echo $(($1 / $3)) ;;
   *) echo "Unsupported operator: $2"; exit 1 ;;
 esac
