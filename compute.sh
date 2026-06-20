b70063a94e2fa1551325daa7bc95cce65a78b9fa
elisha.lye <elisha.lye@rea-group.com>
2026-06-20 11:16:07 +1000

    Add minus

--- compute.sh
+++ compute.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-if [ $# -ne 3 ] || [ "$2" != "+" ]; then
-  echo "Usage: $0 <num1> + <num2>"
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <num1> <+|-> <num2>"
   exit 1
 fi
 
-echo $(($1 + $3))
+case "$2" in
+  +) echo $(($1 + $3)) ;;
+  -) echo $(($1 - $3)) ;;
+  *) echo "Unsupported operator: $2"; exit 1 ;;
+esac
